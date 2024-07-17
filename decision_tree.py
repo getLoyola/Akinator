6eacc7887d17b67b740b59a39b87cab8bd34e499
getLoyola <83439793+getLoyola@users.noreply.github.com>
2024-07-17 11:25:22 -0400

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -3,6 +3,20 @@ class TreeNode:
         self.question = question
         self.yes_branch = yes_branch
         self.no_branch = no_branch
+        self.yes_count = 0
+        self.no_count = 0
 
     def is_leaf(self):
         return self.yes_branch is None and self.no_branch is None
+
+    def update_counts(self, answer):
+        if answer == 'yes':
+            self.yes_count += 1
+        elif answer == 'no':
+            self.no_count += 1
+
+    def get_score(self):
+        total = self.yes_count + self.no_count
+        if total == 0:
+            return 0
+        return min(self.yes_count, self.no_count) / total
